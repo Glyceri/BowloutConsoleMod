a53d9e573ae76c3aa1d71151c04bf77fb89ac8bd
Amber Kortier <AmberKortier@gmail.com>
2024-01-18 13:03:35 +0100

    Initial Commit

---
+++ BowloutConsoleMod/BowloutConsoleMod.cs
@@ -0,0 +1,53 @@
+﻿using BowloutModManager.BowloutMod;
+using BowloutModManager.BowloutMod.Interfaces;
+using System;
+
+namespace BowloutConsoleMod
+{
+    public class BowloutConsoleMod : IBowloutMod
+    {
+        public string Name => "Bowlout Console Mod";
+
+        public Version Version => new Version(1, 0, 0, 0);
+
+        public string Description => "An Error Console for Bowlout.";
+
+        public IBowloutConfiguration Configuration { get; private set; }
+        public BowloutConsoleModConfiguration SampleModConfiguration => (BowloutConsoleModConfiguration)Configuration;
+
+        public void OnSetup()
+        {
+            Configuration = this.GetConfiguration<BowloutConsoleModConfiguration>() ?? new BowloutConsoleModConfiguration();
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public void OnEnable()
+        {
+
+        }
+
+        public void OnDisable()
+        {
+            
+        }
+
+        public void OnUpdate()
+        {
+
+        }
+
+        public void OnFixedUpdate()
+        {
+            
+        }
+
+        public void OnLateUpdate()
+        {
+            
+        }
+    }
+}
---
+++ BowloutConsoleMod/BowloutConsoleModConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using BowloutModManager.BowloutMod.Interfaces;
+
+namespace BowloutConsoleMod
+{
+    public class BowloutConsoleModConfiguration : IBowloutConfiguration
+    {
+        public int Version { get => 1; set => _ = value; }
+    }
+}
